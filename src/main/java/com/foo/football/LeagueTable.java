64c972715a7f55110337187ca3265e0126af1d36
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2019-02-10 11:57:47 +0000

    Add initial code

---
+++ src/main/java/com/foo/football/LeagueTable.java
@@ -0,0 +1,14 @@
+package com.foo.football;
+
+import java.util.List;
+
+public class LeagueTable {
+
+	public LeagueTable(final List<Match> matches) {
+	}
+
+	public List<LeagueTableEntry> getTableEntries() {
+		return null;
+	}
+
+}
